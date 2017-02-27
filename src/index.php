3a3d9ef86992b0e35ca8b53c08a35235c838f89b
dragorane <loic.laisne@gmail.com>
2017-02-27 09:22:50 +0000

    add content & left column

--- src/index.php
+++ src/index.php
@@ -1,3 +1,13 @@
 <?php
 require("view/header.html");
+echo "<div class='container'>";
+echo "<div class='row'>";
+echo "<div class='col-md-4'>";
+require("view/border_left.html");
+echo "</div>";
+echo "<div class='col-md-8'>";
+require("view/catalogue.html");
+echo "</div>";
+echo "</div>";
+echo "</div>";
 require("view/footer.html");
