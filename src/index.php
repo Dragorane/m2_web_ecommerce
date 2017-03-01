1c2caadc48fb5c52e1605c38f71a13c2763073cf
dragorane <loic.laisne@gmail.com>
2017-03-01 17:11:50 +0000

    dernière version design + controller catalogue

--- src/index.php
+++ src/index.php
@@ -1,13 +1,22 @@
 <?php
-require("view/header.html");
-echo "<div class='container'>";
-echo "<div class='row'>";
-echo "<div class='col-md-4'>";
-require("view/border_left.html");
-echo "</div>";
-echo "<div class='col-md-8'>";
-require("view/catalogue.html");
-echo "</div>";
-echo "</div>";
-echo "</div>";
-require("view/footer.html");
+//This is the route file
+session_start();
+require("controller/Catalogue.php");
+$catalogueController = new Catalogue;
+
+// Routing
+if (isset ($_GET ['page'])) $page = $_GET ['page'];
+if (isset ($_GET ['action'])) $action = $_GET ['action']; else $action = "";
+		
+switch ($page) {
+ 	case "shopping_cart":
+ 		require("php/$page.php");
+		break;
+	case "catalogue":
+		$catalogueController->mainPage();
+		break;
+	default:
+		$catalogueController->mainPage();
+        break;
+}
+
